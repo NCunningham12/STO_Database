b2fc770444ddb08df66b12608407440489d55306
Nathan Cunningham <counterweight22@gmail.com>
2024-07-17 22:49:58 -0700

    Implemented Flask. Needs logic applied to client-side db. Needs CRUD applications

--- STO_database.py
+++ STO_database.py
@@ -1,3 +1,4 @@
+from flask import Flask, render_template
 import mysql.connector
 import os
 from dotenv import load_dotenv
@@ -12,78 +13,97 @@ db = mysql.connector.connect(
 )
 mycursor = db.cursor(buffered=True)
 
-def input_trucks():
-  input_auction = input("Auction Price?:\n")
-  input_province = input("Province?:\n")
-  input_book_cad = input("Canadian Book?:\n")
+# Flask Component
 
-  input_data = (input_auction, input_province, input_book_cad)
+app = Flask(__name__)
 
-  input_query = ("INSERT INTO inventory (auction_price, province, book_cad) VALUES (%s, %s, %s)")
+@app.route("/")
+def index():
+  select_query = "SELECT * FROM inventory"
+  mycursor.execute(select_query)
+  records = mycursor.fetchall()
+  return render_template("index.html", records=records)
 
-  mycursor.execute(input_query, input_data)
+@app.route("/add")
+def add_vehicles():
+  return "Adding vehicles to the database"
 
-  db.commit()
+if __name__ == "__main__":
+  app.run(debug = True)
 
-def calculate_bob():
 
-  exchange = input("Current exchange rate:\n")
+# def input_trucks():
+#   input_auction = input("Auction Price?:\n")
+#   input_province = input("Province?:\n")
+#   input_book_cad = input("Canadian Book?:\n")
 
-  mycursor.execute("SELECT id, auction_price, province, book_cad FROM inventory")
-  trucks = mycursor.fetchall()
+#   input_data = (input_auction, input_province, input_book_cad)
 
-  for row in trucks:
+#   input_query = ("INSERT INTO inventory (auction_price, province, book_cad) VALUES (%s, %s, %s)")
+
+#   mycursor.execute(input_query, input_data)
+
+#   db.commit()
+
+# def calculate_bob():
+
+#   exchange = input("Current exchange rate:\n")
+
+#   mycursor.execute("SELECT id, auction_price, province, book_cad FROM inventory")
+#   trucks = mycursor.fetchall()
+
+#   for row in trucks:
     
-    id = int(row[0])
-    auction_price = float(row[1])
-    province = str(row[2])
-    book_cad = float(row[3])
-    book_usd = round(float(book_cad) * float(exchange), 2)
+#     id = int(row[0])
+#     auction_price = float(row[1])
+#     province = str(row[2])
+#     book_cad = float(row[3])
+#     book_usd = round(float(book_cad) * float(exchange), 2)
 
-    if province.lower() == 'alb' or province.lower() == 'bc':
-      auction_price += 1000
-      auction_price *= 1.05
-      canadian_price = round(auction_price, 2)
+#     if province.lower() == 'alb' or province.lower() == 'bc':
+#       auction_price += 1000
+#       auction_price *= 1.05
+#       canadian_price = round(auction_price, 2)
 
-      us_price = round(float(canadian_price) * float(exchange), 2)
-      updated_us_price = us_price + 2300
-      updated_us_price *= 1.01
-      total_us = round(updated_us_price, 2)
+#       us_price = round(float(canadian_price) * float(exchange), 2)
+#       updated_us_price = us_price + 2300
+#       updated_us_price *= 1.01
+#       total_us = round(updated_us_price, 2)
 
-      bob = round(float(book_usd) - float(total_us), 2)
+#       bob = round(float(book_usd) - float(total_us), 2)
 
-    elif province.lower() == 'ont':
-      auction_price += 1000
-      auction_price *= 1.13
-      canadian_price = round(auction_price, 2)
+#     elif province.lower() == 'ont':
+#       auction_price += 1000
+#       auction_price *= 1.13
+#       canadian_price = round(auction_price, 2)
 
-      us_price = round(float(canadian_price) * float(exchange), 2)
-      updated_us_price = us_price + 3400
-      updated_us_price *= 1.01
-      total_us = round(updated_us_price, 2)
+#       us_price = round(float(canadian_price) * float(exchange), 2)
+#       updated_us_price = us_price + 3400
+#       updated_us_price *= 1.01
+#       total_us = round(updated_us_price, 2)
 
-      bob = round(float(book_usd) - float(total_us), 2)
+#       bob = round(float(book_usd) - float(total_us), 2)
 
-    calculated_values = (book_usd, canadian_price, us_price, total_us, bob, id)
+#     calculated_values = (book_usd, canadian_price, us_price, total_us, bob, id)
 
-    print(calculated_values)
+#     print(calculated_values)
 
-    input_query2 = ("UPDATE inventory SET book_usd = %s, canadian_price = %s, us_price = %s, total_us = %s, bob = %s WHERE id = %s")
+#     input_query2 = ("UPDATE inventory SET book_usd = %s, canadian_price = %s, us_price = %s, total_us = %s, bob = %s WHERE id = %s")
 
-    mycursor.execute(input_query2, calculated_values)
+#     mycursor.execute(input_query2, calculated_values)
 
-  db.commit()
+#   db.commit()
 
-add_values = input("Would you like to add any values to the DB?\n")
+# add_values = input("Would you like to add any values to the DB?\n")
 
 
-if add_values.lower().startswith("y"):
-  input_trucks()
+# if add_values.lower().startswith("y"):
+#   input_trucks()
 
-fill_values = input("Would you like to fill out the DB values?\n")
+# fill_values = input("Would you like to fill out the DB values?\n")
 
-if fill_values.lower().startswith("y"):
-  calculate_bob()
+# if fill_values.lower().startswith("y"):
+#   calculate_bob()
 
 
 mycursor.close()
