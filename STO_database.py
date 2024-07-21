265e614f9e7b6e450da5e28b20b829dcfacd7cf7
Nathan Cunningham <counterweight22@gmail.com>
2024-07-21 02:03:34 -0700

    Reconfigured Bootstrap. Calculate button functional. Modals not working

--- STO_database.py
+++ STO_database.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
-from flask_bootstrap import Bootstrap5
+# from flask_bootstrap import Bootstrap5
 import mysql.connector
 import os
 from dotenv import load_dotenv
@@ -16,13 +16,13 @@ db = mysql.connector.connect(
 app = Flask(__name__)
 app.secret_key = os.getenv("SECRET_KEY")
 
-bootstrap = Bootstrap5(app)
+# bootstrap = Bootstrap5(app)
 
 
 
 @app.route("/")
 def Index():
-  cursor = db.cursor(buffered=True)
+  cursor = db.cursor()
   select_query = "SELECT * FROM inventory"
   cursor.execute(select_query)
   records = cursor.fetchall()
@@ -39,7 +39,7 @@ def insert():
     auction_price = request.form['Auction Price']
     province = request.form['Province']
     book_cad = request.form['Canadian Book']
-    cursor = db.cursor(buffered=True)
+    cursor = db.cursor()
     cursor.execute("INSERT INTO inventory (auction_price, province, book_cad) VALUES (%s, %s, %s)", (auction_price, province, book_cad))
     db.commit()
 
@@ -49,7 +49,7 @@ def insert():
 @app.route('/delete/<string:id_data>', methods = ['GET'])
 def delete(id_data):
   flash("Vehicle Record Has Been Successfully Deleted")
-  cursor = db.cursor(buffered=True)
+  cursor = db.cursor()
   cursor.execute("DELETE FROM inventory WHERE id=%s", (id_data,))
   db.commit()
   return redirect(url_for('Index'))
@@ -62,12 +62,64 @@ def update():
     auction_price = request.form['Auction Price']
     province = request.form['Province']
     book_cad = request.form['Canadian Book']
-    cursor = db.cursor(buffered=True)
+    cursor = db.cursor()
     cursor.execute("""UPDATE inventory SET auction_price=%s, province=%s, book_cad=%s WHERE id=%s""", (auction_price, province, book_cad, id_data))
     flash("Vehicle Data Updated Successfully")
     db.commit()
     return redirect(url_for('Index'))
   
+
+@app.route('/calculate/', methods=['POST'])
+def calculate_bob():
+  cursor = db.cursor()
+
+  exchange = 0.73
+
+  cursor.execute("SELECT id, auction_price, province, book_cad FROM inventory")
+  trucks = cursor.fetchall()
+
+  for row in trucks:
+    
+    id = int(row[0])
+    auction_price = float(row[1])
+    province = str(row[2])
+    book_cad = float(row[3])
+    book_usd = round(float(book_cad) * float(exchange), 2)
+
+    if province.lower() == 'alb' or province.lower() == 'bc':
+      auction_price += 1000
+      auction_price *= 1.05
+      canadian_price = round(auction_price, 2)
+
+      us_price = round(float(canadian_price) * float(exchange), 2)
+      updated_us_price = us_price + 2300
+      updated_us_price *= 1.01
+      total_us = round(updated_us_price, 2)
+
+      bob = round(float(book_usd) - float(total_us), 2)
+
+    elif province.lower() == 'ont':
+      auction_price += 1000
+      auction_price *= 1.13
+      canadian_price = round(auction_price, 2)
+
+      us_price = round(float(canadian_price) * float(exchange), 2)
+      updated_us_price = us_price + 3400
+      updated_us_price *= 1.01
+      total_us = round(updated_us_price, 2)
+
+      bob = round(float(book_usd) - float(total_us), 2)
+
+    calculated_values = (book_usd, canadian_price, us_price, total_us, bob, id)
+
+    input_query2 = ("UPDATE inventory SET book_usd = %s, canadian_price = %s, us_price = %s, total_us = %s, bob = %s WHERE id = %s")
+
+    cursor.execute(input_query2, calculated_values)
+
+  db.commit()
+
+  return redirect(url_for('Index'))
+
 
 
 if __name__ == "__main__":
@@ -88,11 +140,12 @@ if __name__ == "__main__":
 #   db.commit()
 
 # def calculate_bob():
+#   cursor = db.cursor()
 
 #   exchange = input("Current exchange rate:\n")
 
-#   mycursor.execute("SELECT id, auction_price, province, book_cad FROM inventory")
-#   trucks = mycursor.fetchall()
+#   cursor.execute("SELECT id, auction_price, province, book_cad FROM inventory")
+#   trucks = cursor.fetchall()
 
 #   for row in trucks:
     
@@ -132,7 +185,7 @@ if __name__ == "__main__":
 
 #     input_query2 = ("UPDATE inventory SET book_usd = %s, canadian_price = %s, us_price = %s, total_us = %s, bob = %s WHERE id = %s")
 
-#     mycursor.execute(input_query2, calculated_values)
+#     cursor.execute(input_query2, calculated_values)
 
 #   db.commit()
 
